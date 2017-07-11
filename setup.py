ed0763c0c77dd1d5d71b8b7f1345200df0e0cce4
Simen Daehlin <simen.dehlin@gmail.com>
2017-07-11 17:35:29 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
  
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
- 
+
+
 setup(
     name='reusable-blog-app',
     version='1.0.0',
