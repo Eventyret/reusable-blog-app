2d7b345a29d0bd79a79bdded81ec3e3b5906b71f
Simen Daehlin <simen.dehlin@gmail.com>
2017-07-11 17:51:18 +0100

    updated to 1.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='reusable-blog-app',
-    version='1.0.0',
+    version='1.0.1',
     packages=['reusable_blog'],
     include_package_data=True,
     license='BSD License',  # example license
